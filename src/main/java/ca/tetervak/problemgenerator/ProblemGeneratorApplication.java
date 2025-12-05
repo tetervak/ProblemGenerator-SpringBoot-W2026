cb523b96402292b0529a6fa74301dad50b6b7e06
Alex Tetervak <tetervak@sheridancollege.ca>
2025-12-05 11:49:29 -0500

    fresh project

---
+++ src/main/java/ca/tetervak/problemgenerator/ProblemGeneratorApplication.java
@@ -0,0 +1,13 @@
+package ca.tetervak.problemgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/tetervak/problemgenerator/ProblemGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.tetervak.problemgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
