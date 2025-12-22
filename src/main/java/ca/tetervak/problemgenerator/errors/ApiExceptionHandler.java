c18a0bb5f31ddb0ccddbf91a672d52b268da5ada
Alex Tetervak <tetervak@sheridancollege.ca>
2025-12-22 10:47:34 -0500

    cleaned 404 error handling

--- src/main/java/ca/tetervak/problemgenerator/errors/ApiExceptionHandler.java
+++ src/main/java/ca/tetervak/problemgenerator/errors/ApiExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice(assignableTypes = ApiController.class)
 public class ApiExceptionHandler {
@@ -18,17 +17,6 @@ public class ApiExceptionHandler {
     public ApiError handleIllegalArgumentException(IllegalArgumentException ex) {
         return new ApiError(
                 HttpStatus.BAD_REQUEST.value(),
-                ex.getClass().getSimpleName(),
-                ex.getMessage()
-        );
-    }
-
-    @ExceptionHandler(value = NoResourceFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ApiResponse(content = @Content(mediaType = "application/json"))
-    public ApiError handleNoResourceFoundException(NoResourceFoundException ex) {
-        return new ApiError(
-                HttpStatus.NOT_FOUND.value(),
                 ex.getClass().getSimpleName(),
                 ex.getMessage()
         );
